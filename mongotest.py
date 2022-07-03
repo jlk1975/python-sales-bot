405a298fe6c8a234db2d602a6c3efa5db9a33704
jlk <jkendall1975@gmail.com>
2022-07-03 14:48:29 -0400

    testing mint bot on Sun Jul  3 02:48:29 PM EDT 2022

--- mongotest.py
+++ mongotest.py
@@ -2,9 +2,18 @@ from pymongo import MongoClient
 import pprint
 import datetime
 
+post = {"author": "JLK",
+        "text": "Blog Post 3",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()}
+
 client = MongoClient('mongodb://localhost:27017/')
 db = client.test_database
 collection = db.test_collection
-posts = db.posts
 db.list_collection_names()
-pprint.pprint(posts.find_one())
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+post_id
+# pprint.pprint(posts.find_one())
+pprint.pprint(posts.find_one({"author": "Mike"}))
+pprint.pprint(posts.find_one({"author": "JLK"}))
